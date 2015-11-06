5097219ae7edc34f6ca41b8f6f99aafb4154f3b8
lnberry <lnberry@compute-linux01.cs.ucalgary.ca>
2015-11-05 20:13:59 -0700

    changed switch statement to if else construct add a scanner for user input

--- ObjectCreator.java
+++ ObjectCreator.java
@@ -1,7 +1,47 @@
+import java.util.*;
+
 public class ObjectCreator
 {
-	public Object createObject()
+	public ObjectCreator()
+	{}
+	
+	public Object CreateObject()
 	{
+		//textbased menu and user input
+		System.out.println("what would youlike to do?\n 1)create a simple object\n 2)create a collection of Objects");
+		Scanner darkly = new Scanner(System.in);
+		String userInput=darkly.nextLine();
+		Object object = null;
+		if(userInput.toInteger()==1)
+		{
+			object= createSimpleObject();
+		}
+		else if (userInput.toInteger()==2)
+		{
+			object=createObjectCollectionObject();
+			break;
+		}
+		Sender.connected=false;
+		break;
+		return object;
 	}
-
+	
+	private simpleObject createSimpleObject()
+	{
+		System.out.println("enter value for field")
+		
+		return SimpleObject(a,b);
+	}
+	
+	private ObjectCollectionObject create ObjectsCollectionObject()
+	{
+		Vector<object> list = new Vector<object>();
+		int collectionSize=GUI.getIntInput(......);
+		
+		for (int i=0; i< collectionSize; i++)
+		{
+			list.add(createObject());
+		}
+		return new ObjectsCollectionObject(list);
+	}
 }
